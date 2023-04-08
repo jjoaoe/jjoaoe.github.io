8f8e1f4481b7d73577c01d2e5f4fc42c3fe6f66c
jjoaoe <46454728+jjoaoe@users.noreply.github.com>
2023-04-08 15:52:16 -0300

    Update contas.js

--- js/contas.js
+++ js/contas.js
@@ -58,7 +58,7 @@ function get_value_and_plan() {
     
     // Conta FA Ranked Liberada
     else if (partidas == '2X73zVH') {
-        var valor = 'R$59'
+        var valor = 'Esgotado'
     }
     
     else if (partidas == 'FerroPlus') {

380958ee4f6a56b3939e42ba05fbb177f9e04682
jjoaoe <46454728+jjoaoe@users.noreply.github.com>
2023-08-01 02:06:01 -0300

    Update contas.js

--- js/contas.js
+++ js/contas.js
@@ -1,25 +1,54 @@
 // Define product data
 const products = {
-  "Conta Smurf Bronze": {price: 40, id: "1"},
-  "Conta Smurf Prata": {price: 50, id: "2"},
-  "Conta Smurf Ouro": {price: 60, id: "3"},
-  "Conta Smurf Platina": {price: 70, id: "4"},
-  "Conta Smurf Diamante": {price: 80, id: "5"},
-  "Conta Smurf Mestre": {price: 90, id: "6"},
-  "Conta Smurf Grão-Mestre": {price: 100, id: "7"},
-  "Conta Smurf Desafiante": {price: 110, id: "8"},
+  "Conta Sem Elo (Ranked Liberada)": {
+    price: 14.90, 
+    id: "17j16Hw"
+  },
+  "Conta Smurf Ferro": {
+    price: 99, 
+    id: "1WyQRsC"
+  },
+  "Conta Smurf Bronze": {
+    price: 99, 
+    id: "2pNZ38n"
+  },
+  "Conta Smurf Prata": {
+    price: 99, 
+    id: "1nbWDSn"
+  },
+  "Conta Smurf Ouro": {
+    price: 99, 
+    id: "2Se3YAB"
+  },
+  "Conta Smurf Platina": {
+    price: 99, 
+    id: "122YuJR"
+  },
+  "Conta Smurf Diamante": {
+    price: 99, 
+    id: "2CXXrF4"
+  },
+  "Conta Smurf Ascendente": {
+    price: 99, 
+    id: "1SaFdAY"
+  },
+  "Conta Sem Elo FULL ACCESS (Ranked Liberada)": {
+    price: 149, 
+    id: "2X73zVH"
+  },
 };
 
 function myFunction() {
   // Get selected product
-  const selectedProduct = document.getElementById("mySelect").value;
+  const selectedProduct = document.getElementById("listaContas").value;
 
   // Find product data
   const productData = products[selectedProduct];
 
   // Update price display
+  // You will need to add the actual prices for the products
   document.getElementById("demo").innerHTML = "R$ " + productData.price;
 
   // Update buy now button
-  document.getElementById("comprar").href = "https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=" + productData.id;
+  document.getElementById("comprar").href = "https://mpago.la/" + productData.id;
 }
